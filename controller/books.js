3c3bf503f84c1d28b761906a3cf641d5e36230c0
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-08-01 00:57:36 +1000

    edit and delete functions (not implemented actually)

--- controller/books.js
+++ controller/books.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var render = require('../config/render');
+var parse = require('co-body');
 
 var book = require('../model/book');
 
@@ -11,23 +12,26 @@ module.exports = {
     var books = book.all();
     this.body = yield render('book/books.html', { books: books });
   },
-  show: function* (id) {
-    var book = book.find(id);
-    this.body = book;
+  new: function* () {
+    this.body = yield render('book/book-add.html');
   },
   create: function* () {
     var b = yield parse(this);
     book.insert(b.title, b.author);
-    this.list();
+    this.redirect('/');
   },
   edit: function* (id) {
+    var b = book.find(id);
+    this.body = yield render('book/book-edit.html', { book: b });
+  },
+  modify: function* (id) {
     var b = yield parse(this);
     book.update(id, b.title, b.author);
-    this.list();
+    this.redirect('/');
   },
   remove: function* (id) {
     book.delete(id);
-    this.list();
+    this.redirect('/');
   }
 
 };
--- routes.js
+++ routes.js
@@ -9,9 +9,10 @@ module.exports = function routes(app) {
   // routes
   app.use(route.get('/', books.list));
   app.use(route.get('/book', books.list));
-  app.use(route.get('/book/:id', books.show));
+  app.use(route.get('/book/new', books.new));
   app.use(route.post('/book', books.create));
-  app.use(route.post('/book/:id/edit', books.edit));
-  app.use(route.post('/book/:id/remove', books.remove));
+  app.use(route.get('/book/:id/edit', books.edit));
+  app.use(route.post('/book/:id', books.modify));
+  app.use(route.get('/book/:id/remove', books.remove));
 
 };
